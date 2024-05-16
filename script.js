e0f1a3979e6da7e32173a049073fe5bbe6e5f282
David Sikora <code@davidsikora.xyz>
2024-05-16 14:12:45 -0400

    Update script to use GIS_FeatureKey for park names

--- script.js
+++ script.js
@@ -6,27 +6,35 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Add markers or other features here
-
+// Function to add GeoJSON data to the map
+function addGeoJsonData(url, map, nameProperty, additionalProperty) {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            L.geoJSON(data, {
+                onEachFeature: function (feature, layer) {
+                    if (feature.properties && feature.properties[nameProperty]) {
+                        let popupContent = `<b>${feature.properties[nameProperty]}</b>`;
+                        if (feature.properties[additionalProperty]) {
+                            popupContent += `<br>${feature.properties[additionalProperty]}`;
+                        }
+                        layer.bindPopup(popupContent);
+                    }
+                }
+            }).addTo(map);
+        })
+        .catch(error => {
+            console.error(`Failed to load GeoJSON data from ${url}:`, error);
+        });
+}
 
 // Add GeoJSON data for parks
-fetch('data/Parks.geojson')
-    .then(response => response.json())
-    .then(data => {
-        L.geoJSON(data, {
-            onEachFeature: function (feature, layer) {
-                layer.bindPopup(`<b>${feature.properties.name}</b><br>${feature.properties.description}`);
-            }
-        }).addTo(map);
-    });
+addGeoJsonData('data/Parks.geojson', map, 'GIS_FeatureKey', 'description');
 
 // Add GeoJSON data for park amenities
-fetch('data/Park_Amenities.geojson')
-    .then(response => response.json())
-    .then(data => {
-        L.geoJSON(data, {
-            onEachFeature: function (feature, layer) {
-                layer.bindPopup(`<b>${feature.properties.name}</b><br>${feature.properties.type}`);
-            }
-        }).addTo(map);
-    });
+addGeoJsonData('data/Park_Amenities.geojson', map, 'name', 'type');
